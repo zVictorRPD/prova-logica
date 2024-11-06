797bdf4da8fb7e42264760a63958112cd0e05518
Victor Martins <victor2007azevedo@hotmail.com>
2024-11-06 18:46:23 -0300

    feat: Tarefa 1 - Gerando a coluna de IDH
    Criando método para adicionar o campo de IDH e para gerar o número

--- TAREFA1/Program.js
+++ TAREFA1/Program.js
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 class CSVParser {
     constructor(filePath) {
@@ -9,7 +9,7 @@ class CSVParser {
 
     // Lê o arquivo
     readFile() {
-        return readFileSync(this.filePath, 'utf8');
+        return readFileSync(this.filePath, "utf8");
     }
 
     // Removendo aspas adicionais e quebras de linha
@@ -17,17 +17,22 @@ class CSVParser {
         return value.replace(/["\r]/g, "");
     }
 
+    // Função para gerar um número aleatório para o IDH
+    generateRandomIDH() {
+        return (Math.random() * 0.4 + 0.5).toFixed(1);
+    }
+
     // Utilizado para normalizar os dados do arquivo e salvar em um array
     processFile() {
         const fileContent = this.readFile();
-        
-        fileContent.split('\n').forEach((line, index) => {
+
+        fileContent.split("\n").forEach((line, index) => {
             if (index === 0) {
-                this.headers = line.split('; ').map(this.regexToFormatString);
+                this.headers = line.split("; ").map(this.regexToFormatString);
                 return;
             }
-            
-            const [nameRaw, populationRaw] = line.split(';');
+
+            const [nameRaw, populationRaw] = line.split(";");
             const name = this.regexToFormatString(nameRaw);
             const population = Number(populationRaw);
 
@@ -40,19 +45,28 @@ class CSVParser {
         });
     }
 
+    // Inserindo a nova coluna de IDH
+    addIDHColumn() {
+        this.headers.push("IDH");
+        this.lines = this.lines.map((line) => {
+            return {
+                ...line,
+                IDH: this.generateRandomIDH(),
+            };
+        });
+    }
+
     // Retorna os dados
     getData() {
         return {
             headers: this.headers,
-            lines: this.lines
+            lines: this.lines,
         };
     }
 }
 
-
-const parser = new CSVParser('mapa.csv');
+const parser = new CSVParser("mapa.csv");
 parser.processFile();
+parser.addIDHColumn();
 const data = parser.getData();
 console.log(data);
-
-
